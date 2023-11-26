b388ffb9e141e6bd81312a1f65455977f1e0eb4c
Pranzal_360 <pranzalkhatiwada03@gmail.com>
2023-11-26 17:40:33 +0545

    has updated the timestamp so that daily basis ma time ko issue naaosh such as having same time yesterday and todya because date haru thena only hours and minute

--- additional_funcs.py
+++ additional_funcs.py
@@ -2,8 +2,7 @@ from datetime import datetime, timedelta
 
 def set_expirey():
     current_time = datetime.now()
-    print(current_time)
     expirey_time = current_time + timedelta(hours=1)
-    expirey_time_str = expirey_time.strftime("%H%M%S")
+    expirey_time_str = expirey_time.strftime("%H%M%S%f")
     return int(expirey_time_str)
 
