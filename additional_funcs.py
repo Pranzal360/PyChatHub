f1d29bed04f6fda4b55ce5ebd9bfaf3855849bca
Pranzal_360 <pranzalkhatiwada03@gmail.com>
2023-12-02 19:06:31 +0545

    expirey time has also been changed so that there will be no time confilcts, further testing maybe required

--- additional_funcs.py
+++ additional_funcs.py
@@ -3,6 +3,6 @@ from datetime import datetime, timedelta
 def set_expirey():
     current_time = datetime.now()
     expirey_time = current_time + timedelta(hours=1)
-    expirey_time_str = expirey_time.strftime("%H%M%S%f")
+    expirey_time_str = expirey_time.strftime("%Y%m%d%H%M%S%f")
     return int(expirey_time_str)
 
