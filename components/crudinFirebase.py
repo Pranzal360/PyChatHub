bb8b1018696430f59726fde49dcb1bd94603bf2a
Pranzal360 <pranzalkhatiwada03@gmail.com>
2025-12-23 19:12:53 +0545

    Removed the database creds and added environment variables for safe practice

--- components/crudinFirebase.py
+++ components/crudinFirebase.py
@@ -1,15 +1,18 @@
 from pyrebase import initialize_app
 from time import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 config = {
-    "apiKey": "",
-    "authDomain": "",
-    "databaseURL": "",
-    "projectId": "",
-    "storageBucket": "",
-    "messagingSenderId": "",
-    "appId": "",
-    "databaseURL": "",
+    "apiKey": os.getenv("API_KEY"),
+    "authDomain": os.getenv("AUTH_DOMAIN"),
+    "projectId": os.getenv("PORJECT_ID"),
+    "storageBucket": os.getenv("STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
+    "appId": os.getenv("APP_ID"),
+    "databaseURL": os.getenv("DATABASE_URL"),
 }
 
 
